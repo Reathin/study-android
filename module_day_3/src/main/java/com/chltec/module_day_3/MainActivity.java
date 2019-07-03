8508304647f7bef029f01ed77636d4104d9961e1
Rair <rairmmd@qq.com>
2019-07-03 20:56:08 +0800

    ...

---
+++ module_day_3/src/main/java/com/chltec/module_day_3/MainActivity.java
@@ -0,0 +1,36 @@
+package com.chltec.module_day_3;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        String text = getString(R.string.text);
+        String text1 = getResources().getString(R.string.text);
+
+        int color1 = Color.parseColor("#ffffff");
+        int color = ContextCompat.getColor(this, R.color.colorAccent);
+
+        TextView textView = new TextView(this);
+        textView.setHeight(120);
+        textView.setWidth(100);
+        textView.setText("动态添加");
+        textView.setGravity(Gravity.CENTER);
+        textView.setBackgroundResource(R.drawable.button_selector);
+
+        LinearLayout llContainer = findViewById(R.id.ll_container);
+        llContainer.addView(textView);
+
+    }
+}
---
+++ module_day_3/src/main/java/com/chltec/module_day_3/Test.java
@@ -0,0 +1,18 @@
+package com.chltec.module_day_3;
+
+import android.content.Context;
+
+/**
+ * @author Rair
+ * @date 2019/7/3
+ * <p>
+ * desc:
+ */
+public class Test {
+
+    public void zifuchuan(Context context) {
+        String text = context.getString(R.string.text);
+    }
+
+
+}
