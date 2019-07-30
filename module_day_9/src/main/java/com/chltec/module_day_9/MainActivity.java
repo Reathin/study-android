af9592d8200c7d446ba8697c55fc2d44fef01d4d
Rair <rairmmd@qq.com>
2019-07-30 20:43:47 +0800

    ...

---
+++ module_day_9/src/main/java/com/chltec/module_day_9/MainActivity.java
@@ -0,0 +1,35 @@
+package com.chltec.module_day_9;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_1).setOnClickListener(this);
+        findViewById(R.id.btn_2).setOnClickListener(this);
+        findViewById(R.id.btn_3).setOnClickListener(this);
+        findViewById(R.id.btn_4).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_1:
+                break;
+            case R.id.btn_2:
+                break;
+            case R.id.btn_3:
+                break;
+            case R.id.btn_4:
+                break;
+            default:
+                break;
+        }
+    }
+}
